2b6994f99fec84b14b864ec4e51111706e84c413
Alexandru <alexandru.ciresnea@stfc.ac.uk>
2020-02-20 17:23:44 +0000

    start reader

--- src/reader.java
+++ src/reader.java
@@ -1,2 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class reader {
+    public void read() throws FileNotFoundException {
+        File file = new File();
+        Scanner scanner = new Scanner(file);
+
+    }
 }
